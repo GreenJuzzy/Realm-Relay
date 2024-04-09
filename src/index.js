468ba8b5ab52fb03af80ac6f928b6dd4a4e96cc5
GreenJuzzy <65908209+GreenJuzzy@users.noreply.github.com>
2024-04-09 23:40:40 +0200

    Add from realm to discord

--- src/index.js
+++ src/index.js
@@ -8,25 +8,28 @@ config()
 const bot = bedrockProtocol.createClient({
     profilesFolder: "./profiles",
     realms: {
-        pickRealm: (realm) => realm.find(r => r.name.replace(/§\w/g, "").toLowerCase().includes(process.env.realmName))
+        pickRealm: (realms) => realms.find(realm => process.env.realm_id ? process.env.realm_id == realm.id : realm.name.replace(/§\w/g, "").includes(process.env.realm_name)).id
     }
 })
 
+
 const client = new discord.Client({
-    intents: ["MessageContent"],
+    intents: ["MessageContent", "Guilds", "GuildMessages"],
     allowedMentions: []
 })
 
+client.on("ready", () => console.log("Discord ready"))
+bot.on("spawn", () => console.log("Bot spawned!"))
 
 client.on("messageCreate", (message) => {
+    if (message.channel.id !== process.env.channel_id || message.author.bot) return;
 
-    if (message.channel.id !== process.env.channel_id && !message.author.bot) return;
-
-    const formattedMessage = `§8[§9Discord§8] §7${message.author.username} § §8>§f${message.content.replace(/§\w/g, "")}`
+    const formattedMessage = `§8[§9Discord§8] §8[§f${message.member.roles.highest.name}§8] §7${message.author.username} § §8> §f${message.content.replace(/§\w/g, "")}`
 
     const randomUUID = uuid.v4()
+
     bot.queue(`command_request`, {
-        command: `tellraw @a {"rawtext":[{"text":"${formattedMessage}}]}`,
+        command: `/tellraw @a {"rawtext":[{"text":"${formattedMessage}"}]}`,
         origin: {
             type: "player",
             request_id: randomUUID,
@@ -35,10 +38,32 @@ client.on("messageCreate", (message) => {
         interval: false,
         version: 70
     })
-
 })
 
+bot.on("text", async (packet) => {
+    const channel = await client.channels.fetch(process.env.channel_id)
+    
+    if (packet.type == "chat") {
+
+        try {
+            var message = `${packet.source_name}: ${packet.message.replace(/§\w/g, "")}`
+            channel.send({ content: `${message}`, allowedMentions: { parse: [] } })
+        } catch (e) {
+            console.log({ error: e.message, data: packet })
+        }
+
+    } else if (packet.type == "json") {
+
+        try {
+            var message = JSON.parse(packet.message).rawtext[0].text.replace(/§[0-9a-z]/g, "")
+            channel.send({ content: `${message}`, allowedMentions: { parse: [] } })
+        } catch (e) {
+            console.log({ error: e.message, data: packet })
+        }
+
+    }
 
 
+})
 
 client.login(process.env.token)
